8ff1d6c6a93f3213ef44654ce0b7a46b8953d7be
NayleDiouri <93705247+NayleDiouri@users.noreply.github.com>
2023-12-13 18:18:26 +0000

    essais

---
+++ Assets/scripts/SpawnArtefacts.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArtefacts : MonoBehaviour
+{
+    public SpriteRenderer SpriteRenderer;
+    void Start()
+    {
+        
+    }
+
+
+
+}
